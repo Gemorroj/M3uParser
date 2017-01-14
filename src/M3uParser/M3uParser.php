03823710cbdd3709d0c0a41d229e1057a26863e5
Gemorroj <wapinet@gmail.com>
2017-01-14 10:38:06 +0300

    Change license gpl 3.0 to lgpl 3.0

--- src/M3uParser/M3uParser.php
+++ src/M3uParser/M3uParser.php
@@ -1,14 +1,4 @@
 <?php
-/**
- *
- * This software is distributed under the GNU GPL v3.0 license.
- *
- * @author    Gemorroj
- * @copyright 2015 http://wapinet.ru
- * @license   http://www.gnu.org/licenses/gpl-3.0.txt
- * @link      https://github.com/Gemorroj/M3uParser
- *
- */
 
 namespace M3uParser;
 
